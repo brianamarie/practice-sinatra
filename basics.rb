384ed86dc9ab3966eb61225b7b6e4a995ad15256
Briana Swift <brianamarie@github.com>
2016-09-07 12:23:02 -0500

    add exec in front of command

--- basics.rb
+++ basics.rb
@@ -5,7 +5,7 @@ require 'json'
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
-  "addcollaborators -r brianamarie/travel-log -i 8"
+  exec 'addcollaborators -r brianamarie/travel-log -i 8'
 end
 
 get '/' do
