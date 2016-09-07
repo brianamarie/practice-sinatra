c414b3ea45cff1f3a743c60c0cade7a8dfc1d3d9
Briana Swift <brianamarie@github.com>
2016-09-07 11:18:53 -0500

    Create basics.rb

---
+++ basics.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+
+post '/payload' do
+  push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+  addcollaborators -r brianamarie/travel-log -i 8
+end
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/about' do
+  "I'm Briana and I love waffles."
+end
+
+get '/hello/:name/' do
+  "Hello there, #{params[:name]}."
+end
+
+get '/hello/:name/:city' do
+  "Hey there #{params[:name]} from #{params[:city]}."
+end
