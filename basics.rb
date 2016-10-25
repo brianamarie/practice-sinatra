c135fec65ec1e78c3f5c389547014c0eede00a37
Briana Swift <brianamarie@github.com>
2016-10-24 21:35:29 -0500

    Update for oct 25 class

--- basics.rb
+++ basics.rb
@@ -10,7 +10,7 @@ post '/payload' do
   puts "I got some JSON: #{push.inspect}"
   jsontest = "I got some JSON: #{push.inspect}"
   Collaborator.add repo_name: "githubschool/open-enrollment-classes-introduction-to-github", issue_num: 927
-  Collaborator.add repo_name: "githubteacher/oct-20", issue_num: 1
+  Collaborator.add repo_name: "githubteacher/oct-25", issue_num: 1
 end
 
 get '/' do
